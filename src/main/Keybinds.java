9f1d0ce5d8c96a27e7ed03ba15b5739c9ac1602b
Scott Forsmann <forsmannscott@gmail.com>
2023-04-05 20:27:38 -0700

    Updated Keybinds

--- src/main/Keybinds.java
+++ src/main/Keybinds.java
@@ -1,14 +1,15 @@
 package main;
 
+import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Keybinds{
-	final int numKeybinds = 5; //TODO Change me if we add more keybinds!
-	String[] keybinds = new String[5];
+	int[] keybinds = new int[5]; // array of KeyEvent.VK_[key] values (aka ints)
 	Hashtable<String, Integer> actionToIndex = new Hashtable<String, Integer>();
 	String playername;
 	String filePath;
@@ -17,7 +18,7 @@ public class Keybinds{
 	FileWriter writer;
 	
 	public Keybinds(String playerName){
-		filePath = "data/keybind_" + playerName+ ".txt";
+		filePath = "data/keybind_" + playerName + ".txt";
 		file = new File(filePath);
 		if (file.exists())
 			loadKeybinds(playerName);
@@ -25,14 +26,52 @@ public class Keybinds{
 			generateDefaultKeybinds();
 			saveKeybinds();
 		}
+		initializeActionToIndex();
+	}
+	
+	private void initializeActionToIndex() {
+		actionToIndex.put("moveLeft", 0);
+		actionToIndex.put("moveRight", 1);
+		actionToIndex.put("crouch", 2);
+		actionToIndex.put("jump", 3);
+		
+		actionToIndex.put("attack", 4);
+	}
+	
+	public void generateDefaultKeybinds() {
+		//FIXME BAD DEFAULT KEYBINDS, CHANGE IF YOU KNOW WHAT FIGHTING GAME KEYBINDS ARE LIKE LOL
+		keybinds[0] = KeyEvent.VK_A; // moveLeft
+		keybinds[1] = KeyEvent.VK_D; // moveRight
+		keybinds[2] = KeyEvent.VK_S; // crouch
+		keybinds[3] = KeyEvent.VK_W; // jump
+		
+		keybinds[4] = KeyEvent.VK_R; // attack
 	}
 	
 	public void loadKeybinds(String playerName) {
-		
+		try{
+			scanner = new Scanner(file);
+			for (int i = 0; i < keybinds.length; i++) { 
+				if (scanner.hasNextInt()) {
+					keybinds[i] = scanner.nextInt();
+				} else {
+					generateDefaultKeybinds();
+					saveKeybinds();
+					break;
+				}
+			}
+			scanner.close();
+		} catch (FileNotFoundException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private int getActionIndex(String action) {
-		return actionToIndex.get(action);
+		if (actionToIndex.get(action) != null)
+			return actionToIndex.get(action);
+		else
+			return -1;
 	}
 	
 	private void saveKeybinds() {
@@ -40,24 +79,20 @@ public class Keybinds{
 		try{
 			file.createNewFile();
 			writer = new FileWriter(filePath);
-			for (int i = 0; i < numKeybinds; i++) {
-				String data = "";
+			for (int i = 0; i < keybinds.length; i++) {
+				String data = Integer.toString(keybinds[i]) + " ";
 				writer.write(data);
 			}
 			writer.close();
 		} catch (IOException e){
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}			
 	}
 	
-	public void generateDefaultKeybinds() {
-		//FIXME BAD DEFAULT KEYBINDS, CHANGE IF YOU KNOW WHAT FIGHTING GAME KEYBINDS ARE LIKE LOL
-		
-	}
-	
-	public boolean setKeybindUnsafe(String action, int keyCode) {
-		
-		return true;
+	public void setKeybindUnsafe(String action, int keyCode) {
+		int actionIndex = getActionIndex(action);
+		if (actionIndex != -1) {
+			keybinds[getActionIndex(action)] = keyCode;			
+		}
 	}
 }
