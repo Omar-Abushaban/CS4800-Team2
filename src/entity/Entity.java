7f5b21f479d643bbac835885078676fce7ba764a
diana <127167244+BlueberryMufffins@users.noreply.github.com>
2023-05-06 23:52:51 -0700

    Update Entity.java

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,10 @@
 package entity;
 
+//diana: putting my name just to keep tabs on 
+//what I put in case I need to go back and change something
+import java.awt.image.BufferedImage;
+
+
 public class Entity{
 	int x, y;
 	int hp;
@@ -7,4 +12,8 @@ public class Entity{
 	
 	public int width = 100;
 	public int height = 200;
+	
+	//diana: for player1 character and actions
+	public BufferedImage attack1, attack2, attack3, dead, hurt, idle, jump, protection, run, walk;
+	public String direction;
 }
